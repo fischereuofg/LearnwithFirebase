9eb0a0fb97eff9273e86179a08c2380cc2f49732
Ethan Fischer <ethanfischer360@gmail.com>
2025-01-20 18:40:09 -0500

    test

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,29 @@
 
 /* Paste  API code here */
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyAfAegDXuEG0SfohPvQz4DPKXi6ONwGGsQ",
+authDomain: "fbtfischer.firebaseapp.com",
+projectId: "fbtfischer",
+storageBucket: "fbtfischer.firebasestorage.app",
+messagingSenderId: "670930940585",
+appId: "1:670930940585:web:3ec8d138331ebb30a16fb8",
+measurementId: "G-QR4RG2C1FS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 // Additional imports to initialize Firebase 
 // NOTE YOU CAN REMOVE THE ANALYTICS variable 
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- scripts/login.js
+++ scripts/login.js
@@ -4,21 +4,24 @@ import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "http
 // Sign up user
 document.getElementById('Sign-Up-Button').addEventListener('click', async function (e) {
     // 4. Prevent the default form submission behavior
-
+    e.preventDefault();
     // 5. Store user inputs in variables
-
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     // 7ab. Store error and success messages in variables
-
-
+    const errorMessage = document.getElementById("error-message");
+    const successMessage = document.getElementById("succese-message");
     // 6. Try and catch logic to create user with email and password
     try {
         // 6a. Use async/await to create user with email and password
-
+        await createUserWithEmailAndPassword(auth, email, password);
         // 8a. Display success message 
-
+        successMessageMessage.textContent="Sign up successful, Login to continue";
+        errorMessage.textContent=' ';
     } catch (error) {
         // 8b. Display error message
-        
+        errorMessage.textContent=error.message;
+        successMessage.textContent='Wrong';
     }
 });
 

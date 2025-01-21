5e7320e73d1a09d0c57ea001d7c26156f6adb60e
Ethan Fischer <ethanfischer360@gmail.com>
2025-01-20 19:18:28 -0500

    Test 2

--- scripts/login.js
+++ scripts/login.js
@@ -6,23 +6,47 @@ document.getElementById('Sign-Up-Button').addEventListener('click', async functi
     // 4. Prevent the default form submission behavior
     e.preventDefault();
     // 5. Store user inputs in variables
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     // 7ab. Store error and success messages in variables
-    const errorMessage = document.getElementById("error-message");
-    const successMessage = document.getElementById("succese-message");
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
     // 6. Try and catch logic to create user with email and password
     try {
         // 6a. Use async/await to create user with email and password
         await createUserWithEmailAndPassword(auth, email, password);
         // 8a. Display success message 
-        successMessageMessage.textContent="Sign up successful, Login to continue";
+        successMessage.textContent='Sign up successful, Login to continue';
         errorMessage.textContent=' ';
     } catch (error) {
         // 8b. Display error message
         errorMessage.textContent=error.message;
-        successMessage.textContent='Wrong';
+        successMessage.textContent=' ';
     }
 });
 
 // 9. Login user
+document.getElementById('Login-Button').addEventListener('click', async function (e) {
+    // 4. Prevent the default form submission behavior
+    e.preventDefault();
+    // 5. Store user inputs in variables
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // 7ab. Store error and success messages in variables
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
+    // 6. Try and catch logic to create user with email and password
+    try {
+        // 6a. Use async/await to create user with email and password
+        await signInWithEmailAndPassword(auth, email, password);
+        // 8a. Display success message 
+        successMessage.textContent='Sign up successful, Login to continue';
+        errorMessage.textContent=' ';
+        window.location.href = "index.html";
+        localStorage.setItem('userEmail', email);
+    } catch (error) {
+        // 8b. Display error message
+        errorMessage.textContent=error.message;
+        successMessage.textContent=' ';
+    }
+});
